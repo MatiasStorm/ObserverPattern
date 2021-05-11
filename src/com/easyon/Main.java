7d40cf618257d6dc16a3cad7ec9d9b52e101bb62
JooergenKoeldal <joer1894@stud.kea.dk>
2021-05-11 15:53:32 +0200

    add comments

--- src/com/easyon/Main.java
+++ src/com/easyon/Main.java
@@ -2,14 +2,23 @@ package com.easyon;
 
 public class Main {
     public static void main(String[] args) {
+
+        //instantiate two new publishers
         Publisher p = new Publisher();
-        new Subscriber(p);
-        new Subscriber(p);
-        new Subscriber(p);
-        new Subscriber(p);
 
         Publisher p2 = new Publisher();
 
+        //Sub 1
+        p.addSubscriber(new Subscriber());
+        //Sub 2
+        p.addSubscriber(new Subscriber());
+        //Sub 3
+        p.addSubscriber(new Subscriber());
+        //Sub 4
+        p2.addSubscriber(new Subscriber());
+
+
+        //Changes the value of the attribute using the method from publisher.
         System.out.println("Change p1's value");
         p.changeAttribute(2);
 
--- src/com/easyon/Subscriber.java
+++ src/com/easyon/Subscriber.java
@@ -1,13 +1,14 @@
 package com.easyon;
 
 public class Subscriber {
+
     private static int counter = 1;
     private int id;
 
-    public Subscriber( Publisher p ){
+    public Subscriber(){
         id = counter++;
-        p.addSubscriber(this);
     }
+
     public void update(int value){
         System.out.printf("Subscriber (%d): got an update with value: %d\n", id, value);
     }
