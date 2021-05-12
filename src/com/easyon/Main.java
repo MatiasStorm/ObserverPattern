a33d31d88b79beea851ebab01bf41ab9f5524aa5
JooergenKoeldal <joer1894@stud.kea.dk>
2021-05-12 10:43:58 +0200

    add comments

--- src/com/easyon/Main.java
+++ src/com/easyon/Main.java
@@ -14,15 +14,18 @@ public class Main {
         p.addSubscriber(new Subscriber());
         //Sub 3
         p.addSubscriber(new Subscriber());
+
         //Sub 4
         p2.addSubscriber(new Subscriber());
 
 
-        //Changes the value of the attribute using the method from publisher.
+        //Changes the value of the attribute using the method from publisher 1.
         System.out.println("Change p1's value");
         p.changeAttribute(2);
-
+        //Changes the value of the attribute using the method from publisher 2.
         System.out.println("\nChange p2's value");
-        p2.changeAttribute(2);
+        p2.changeAttribute(5);
+
+        //Note only the subscribers from p1 (1,2,3) recives the updated value 2, p2's new value is 5.
     }
 }
--- src/com/easyon/Publisher.java
+++ src/com/easyon/Publisher.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 
 public class Publisher {
     private ArrayList<Subscriber> subscribers = new ArrayList<>();
+
+    //This attribute is the information being passed to the subscribers, could be anything.
     private int attribute;
 
+    //Can be used to change the attribute
     public void changeAttribute(int newValue){
         System.out.println("Publisher: Change method is called and the attribute is changed to: " + newValue);
         attribute = newValue;
+        //Uses the another method from publisher
         notifySubscriber(newValue);
     }
-
+        //loops through all subscribers in the ArrayList Subscriber and calls the update function on each
     public void notifySubscriber(int val){
         for(Subscriber s : subscribers){
+            //The val here is replaced with the "newValue" from the change attribute method
             s.update(val);
         }
     }
